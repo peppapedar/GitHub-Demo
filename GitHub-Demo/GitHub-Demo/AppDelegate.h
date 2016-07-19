47423011e79fc626d6feeeafffdace2dc710009f
sucrose <peppapedar@qq.com>
2016-07-19 11:22:00 +0800

    创建一个测试项目

---
+++ GitHub-Demo/GitHub-Demo/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  GitHub-Demo
+//
+//  Created by 王文哲 on 16/7/19.
+//  Copyright © 2016年 wangwenzhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ GitHub-Demo/GitHub-Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitHub-Demo
+//
+//  Created by 王文哲 on 16/7/19.
+//  Copyright © 2016年 wangwenzhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
